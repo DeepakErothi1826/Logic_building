5c0c6fcdf853e95e810e653dd5a9e4b61df98365
Deepak Erothi <deepakerothi7@gmail.com>
2026-01-22 20:25:53 +0530

    Learned how to find a prime number

---
+++ checkPrimeNumber.js
@@ -0,0 +1,20 @@
+
+PrimeNumber =(num) =>{
+    let isprime = true;
+    if(num <= 1){
+        return "Number is not a prime Number"
+    }
+    for(let i=2; i<= num/2; i++){
+        if(num % i ===0){
+             isprime = false;
+        }
+        if(isprime){
+            return `${num} is a prime number`;
+        }
+        else{
+            return `${num} is not a prime number`;
+        }
+    }
+}
+
+console.log(PrimeNumber(11));
